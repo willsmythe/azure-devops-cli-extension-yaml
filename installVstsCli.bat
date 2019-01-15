cc7b96dd8a27d4c79106e5efc2aa9a2952a5cc7b
Will Smythe <wismythe@microsoft.com>
2019-01-15 13:44:11 -0500

    show where

--- installVstsCli.bat
+++ installVstsCli.bat
@@ -1,9 +1,13 @@
 @echo off
-choco install vsts-cli
-call refreshenv
+REM choco install vsts-cli
+REM call refreshenv
+
+echo "Where is VSTS?"
+call where vsts
 
 for /f %%i in ('where vsts') do set cliExecutable=%%i
 echo %cliExecutable%
+
 for %%i in ("%cliExecutable%") do set cliPath=%%~dpi
 echo %cliPath%
 
