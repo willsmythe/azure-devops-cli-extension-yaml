8e605574b76f921773e2b08f1f4498facb4db81a
Will Smythe <wismythe@microsoft.com>
2019-01-15 13:59:22 -0500

    Last updates

--- installVstsCli.bat
+++ installVstsCli.bat
@@ -2,15 +2,8 @@
 choco install vsts-cli
 call refreshenv
 
-REM echo "Where is VSTS?"
-where vsts
-
-SETLOCAL EnableDelayedExpansion 
+SETLOCAL EnableDelayedExpansion
 
 for /f "tokens=*" %%i in ('where vsts') do set cliExecutable=%%i
-echo %cliExecutable%
-
 for /f "tokens=*" %%i in ("%cliExecutable%") do set cliPath=%%~dpi
-echo %cliPath%
-
 echo ##vso[task.prependpath]%cliPath%
